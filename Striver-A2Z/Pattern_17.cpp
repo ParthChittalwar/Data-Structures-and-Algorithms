6b8da192dbd7b977ee1aa8a6535e21be15de75bd
Parth Santosh Chittalwar <69232341+ParthChittalwar@users.noreply.github.com>
2026-08-06 00:59:33 +0530

    Solved: Pattern 17

---
+++ Striver-A2Z/Pattern_17.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void pattern17(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<= n - i; j++){
+                cout<<" ";
+            }
+
+            char ch = 'A';
+            for(int j=1; j<= (2*i - 1); j++){
+                cout<<ch;
+                if(j<i){
+                    ch++;
+                }else{
+                    ch--;
+                }
+            }
+
+            cout<<endl;
+        }
+    }
+};
