3f68d1be332349274a3707759b3d397792b2d5b9
Parth Santosh Chittalwar <69232341+ParthChittalwar@users.noreply.github.com>
2026-08-12 21:43:54 +0530

    Solved: Divisors of a Number

---
+++ Striver-A2Z/Divisors_of_a_Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> divisors(int n) {
+        vector<int> vec;
+        for(int i=1; i<=n; i++){
+            if(n % i == 0){
+                vec.push_back(i);
+            }
+        }
+        return vec;
+       
+    }
+};
