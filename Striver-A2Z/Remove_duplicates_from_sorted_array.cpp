b9a241e53b19b3912e9de3a4ee87bab4ad43f9d3
Parth Santosh Chittalwar <69232341+ParthChittalwar@users.noreply.github.com>
2026-08-23 03:31:08 +0530

    Solved: Remove duplicates from sorted array

---
+++ Striver-A2Z/Remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        set<int> st;
+        for(int i=0; i<n; i++){
+            st.insert(nums[i]);
+        }
+        int index = 0;
+        for(auto x : st){
+            nums[index] = x;
+            index++;
+        }
+        return index;
+    }
+};
