b7b11e18978894ce162664e91eae080d1dfabcc3
Parth Santosh Chittalwar <69232341+ParthChittalwar@users.noreply.github.com>
2026-08-23 21:08:29 +0530

    Solved: Left Rotate Array by K Places

---
+++ Striver-A2Z/Left_Rotate_Array_by_K_Places.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotateArray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        k = k % n;
+
+        vector<int> temp;
+
+        for(int i=0; i<n; i++){
+            temp.push_back(nums[i]);
+        }
+        for(int i=k; i<n; i++){
+            nums[i-k] = nums[i];
+        }
+        for(int i=n-k; i<n; i++){
+            nums[i] = temp[i - (n-k)];
+        }
+    }
+};
